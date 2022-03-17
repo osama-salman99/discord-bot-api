2ddf57f9edc75ff3101c0fccea81b08d95b81a3c
Osama Salman <osama.salman@progressoft.io>
2022-03-17 15:50:28 +0300

    adds NonProcessingMessageFilter tests

---
+++ filters/src/test/java/osmosis/filters/message/NonProcessingMessageFilterTest.java
@@ -0,0 +1,44 @@
+package osmosis.filters.message;
+
+import org.junit.jupiter.api.Test;
+import osmosis.commons.message.ProcessingMessage;
+import osmosis.filters.message.result.FilterResult;
+import osmosis.message.helpers.MessageHelper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NonProcessingMessageFilterTest {
+    @Test
+    void givenApplicableWhenFilterThenReturnSuccessfulWithoutProcessing() {
+        NonProcessingMessageFilter filter = createNonProcessingFilter(true);
+        ProcessingMessage processingMessage = new ProcessingMessage(MessageHelper.createMessage("content"));
+
+        FilterResult result = filter.filter(processingMessage);
+        assertTrue(result.isSuccessful());
+        assertSame(processingMessage, result.getProcessingMessage());
+    }
+
+    @Test
+    void givenInapplicableWhenFilterThenReturnSuccessfulWithoutProcessing() {
+        NonProcessingMessageFilter filter = createNonProcessingFilter(false);
+        ProcessingMessage processingMessage = new ProcessingMessage(MessageHelper.createMessage("content"));
+
+        FilterResult result = filter.filter(processingMessage);
+        assertFalse(result.isSuccessful());
+        assertSame(processingMessage, result.getProcessingMessage());
+    }
+
+    private NonProcessingMessageFilter createNonProcessingFilter(boolean applicable) {
+        return new NonProcessingMessageFilter() {
+            @Override
+            protected boolean isApplicable(ProcessingMessage processingMessage) {
+                return applicable;
+            }
+        };
+    }
+
+    private void assertSame(ProcessingMessage expected, ProcessingMessage actual) {
+        assertEquals(expected.getMessageObject(), actual.getMessageObject());
+        assertEquals(expected.getProcessedMessage(), actual.getProcessedMessage());
+    }
+}
